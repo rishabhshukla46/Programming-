fc36178d4e9778c357e10c3e6afe9e35d7200957
rocccky <rishabhshukla46@gmail.com>
2020-01-16 14:54:10 +0530

    Update LargestNumberwithKswaps.cpp

--- LargestNumberwithKswaps.cpp
+++ LargestNumberwithKswaps.cpp
@@ -1,52 +1,37 @@
 #include<iostream>
 #include<vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-void print_max(vector<int> intarr, int idx, int k){
-    //cout<<"k = "<<k<<endl;
+void solve(string str, int idx, int k, string& p){
     if(k == 0){
-        for(int i : intarr){
-            cout<<i;
-        }
         return;
     }
+
     int max = -1;
     int id;
-    for(int i = idx; i < intarr.size(); i++){
-        //cout<<"i = "<<i<<endl;
-        if(intarr[i] > max){
-            max = intarr[i];
+    for(int i = idx; i < str.length(); i++){
+        if(str[i] > max){
+            max = str[i];
             id = i;
         }
     }
-    if(intarr[idx] < intarr[id]){
-        //cout<<"swap"<<endl;
-        int temp = intarr[idx];
-        intarr[idx] = intarr[id];
-        intarr[id] = temp;
-    }
-    
-    print_max(intarr, idx+1, k-1);
+    swap(str[idx], str[id]);
+    p = str;
+    solve(str, idx+1, k-1, p);
+    swap(str[idx], str[id]);
     return;
 }
 
 int main(){
-    int num, k;
-    cin >> num;
-    cin >> k;
-    vector<int> arr;
-    vector<int> intarr;
-    while(num / 10 != 0){
-        int temp = num % 10;
-        arr.push_back(temp);
-        num /= 10;
-    }
-    arr.push_back(num);    
-    for(int i = arr.size() - 1; i >=0; i--){
-        intarr.push_back(arr[i]);
-    }
+    string str;
+    cin>>str;
+    int k;
+    cin>>k;
+    string p = "";
+    solve(str, 0, k, p);
+    cout<<p<<endl;
 
-    print_max(intarr, 0, k);
     return 0;
 }
