e8bb122bccbd6f51286ee12b1270ba0f2901b9b5
rocccky <rishabhshukla46@gmail.com>
2020-01-17 22:46:54 +0530

    Add files via upload

---
+++ KnightsTour.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+void display(vector<vector<int>>& chess){
+    for(int i = 0; i < chess.size(); i++){
+        for(int j = 0; j < chess[0].size(); j++){
+            cout<<chess[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+bool Isvalid(vector<vector<int>>& chess, int x, int y){
+    if(x<0 || y<0 || x>=chess.size() || y>= chess[0].size() || chess[x][y] != 0){
+        return false;
+    }
+    return true;
+}
+
+bool solve(vector<vector<int>>& chess, vector<vector<int>>& dir, int sr, int sc, int moves){
+    if(moves == chess.size() * chess[0].size()){
+        display(chess);
+        return true;
+    }
+    bool res = false;
+    for(int i = 0; i < dir.size(); i++){
+        int x = sr + dir[i][0];
+        int y = sc + dir[i][1];
+        if(Isvalid(chess, x ,y)){
+            chess[x][y] = moves;
+            res = res || solve(chess, dir, x, y, moves+1);
+            chess[x][y] = 0; 
+        }
+    }
+    return res;
+} 
+
+int main(){
+    int N, sr, sc;
+    cin>>N;
+    cin>>sr;
+    cin>>sc;
+    vector<vector<int>> chess(N, vector<int>(N, 0));
+    vector<vector<int>> dir = {{-2, -1}, {-2, 1}, {-1, 2}, {1, 2}, {2, -1}, {2, 1}, {-1, -2}, {1, -2}};
+
+    if(solve(chess, dir, sr, sc, 1)){
+        cout<<"true";
+    }
+    else{
+        cout<<"false";
+    }
+    //display(chess);
+    return 0;
+}
